3f8bc4f4d30167f86842159f69bfaf48c7ff11cc
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-05 20:13:53 +0530

    add fulfilment text

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const port = process.env.PORT;
 const app= express();
 app.use(express.urlencoded({extended:false}));
 app.post('/',function(req,res){
-     res.send(JSON.stringify({'fulfillmentText':"12:00 pm"}));
+    //  res.send(JSON.stringify({'fulfillmentText':"12:00 pm"}));
+    return res.json(200,
+        {
+            'fulfillmentText':"12:00 pm"
+        });
  })
 app.listen(port,function(err){
     if(err){
