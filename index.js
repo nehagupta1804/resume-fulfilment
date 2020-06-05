bbc02c6bd7045a410a0e0db826ba35803bdff68b
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-05 20:08:06 +0530

    change time 12

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT;
 const app= express();
 app.use(express.urlencoded({extended:false}));
 app.post('/',function(req,res){
-     res.send(JSON.stringify({'fulfillmentText':"11:00 pm"}));
+     res.send(JSON.stringify({'fulfillmentText':"12:00 pm"}));
  })
 app.listen(port,function(err){
     if(err){
