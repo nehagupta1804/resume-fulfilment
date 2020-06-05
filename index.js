fdaf02f5314cc591b3d1f65d1b604e68dc0e325e
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-05 20:01:54 +0530

    add time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT;
 const app= express();
 app.use(express.urlencoded({extended:false}));
 app.post('/',function(req,res){
-     res.send(JSON.stringify({'fulfillmentText':"hello"}));
+     res.send(JSON.stringify({'fulfillmentText':"11:00 pm"}));
  })
 app.listen(port,function(err){
     if(err){
