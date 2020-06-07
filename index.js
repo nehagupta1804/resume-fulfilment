0d9fe523101de90db673b672838b29d066aa0590
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-07 20:02:30 +0530

    print request2

--- index.js
+++ index.js
@@ -27,14 +27,18 @@ app.post('/',function(req,res){
      }
     return res.json(200,
         {
-          "fulfillmentMessages": [
-            {
-              "text": {
-                "text": [result]
-              }
-            }
-          ]
-          
+          // "fulfillmentMessages": [
+          //   {
+          //     "text": {
+          //       "text": [result]
+          //     }
+          //   }
+          // ]
+          "card":{
+            "title":"hi",
+            "subtitle":"hello"
+          },
+          "platform":"FACEBOOK"
             
         });
 
