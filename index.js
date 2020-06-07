e9a1e3f889035c1cc9096d29eec6aa995c219034
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-07 17:37:30 +0530

    print request2

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/',function(req,res){
      var result="";
      for(var i=0;i<jobsArray.length;i++)
      {
-        result+= (i+1).toString() +jobsArray[i].title +" and "+ jobsArray[i].url ;
+        result+= (i+1).toString() +jobsArray[i].title +" and "+ jobsArray[i].url +'  \n';
      }
     return res.json(200,
         {
