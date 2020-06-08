50e526e5fef13a8cbf7dd0411e686205cec10a12
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-08 17:26:26 +0530

    add mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(`mongodb://resume-fulfilment.herokuapp.com/skillsdb`);
+mongoose.connect(`mongodb://localhost/skillsdb`);
 //aqcuire the connection
 const db = mongoose.connection;
 //error
