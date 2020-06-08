c0d2414e71891d415f46956f3ae215c94a86817e
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-08 15:50:59 +0530

    add mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+//require the library
+const mongoose = require('mongoose');
+
+
+mongoose.connect(`mongodb://resume-fulfilment.herokuapp.com/skillsdb`);
+//aqcuire the connection
+const db = mongoose.connection;
+//error
+db.on('error',console.error.bind(console,'error connectiveity to db'));
+//up and running tehn  print the message
+db.once('open',function(){
+    console.log("Successfully connected to db");
+});
+module.exports= db;
