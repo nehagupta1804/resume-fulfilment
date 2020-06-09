8290c964f24509a227cc521ce771d89f8280321f
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-09 12:49:32 +0530

    add mongoose

--- index.js
+++ index.js
@@ -10,44 +10,46 @@ app.use(express.urlencoded({extended:false}));
 app.post('/',function(req,res){
 
   var skill = req.body.queryResult.parameters["skill_name"];
+  var action = req.body.queryResult.action;
+  if(action == "getJobBySkill"){
 
-  https.get("https://jobs.github.com/positions.json?description="+skill+"&location=new+york", (resp) => {
-  let data = '';
+    https.get("https://jobs.github.com/positions.json?description="+skill+"&location=new+york", (resp) => {
+    let data = '';
 
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
 
-  resp.on('end', () => {
-     
-     var jobsArray =  JSON.parse(data);
-     var result="";
-     for(var i=0;i<jobsArray.length;i++)
-     {
-        result+= (i+1).toString()+jobsArray[i].title +" and "+ jobsArray[i].url +"  \n";
-     }
-    return res.json(200,
-        {
-          "fulfillmentMessages": [
-            {
-              "text": {
-                "text": [result]
+    resp.on('end', () => {
+      
+      var jobsArray =  JSON.parse(data);
+      var result="";
+      for(var i=0;i<jobsArray.length;i++)
+      {
+          result+= (i+1).toString()+jobsArray[i].title +" and "+ jobsArray[i].url +"  \n";
+      }
+      return res.json(200,
+          {
+            "fulfillmentMessages": [
+              {
+                "text": {
+                  "text": [result]
+                }
               }
-            }
-          ]
-            
-        });
+            ]
+              
+          });
 
+      
     
-   
-  });
+    });
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+    
+  }
 });
-   
-   
- })
 app.listen(port,function(err){
     if(err){
        console.log("Error in running server");
