1edae415b3679cf415c4c6c0024bde3d0db343d2
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-10 20:00:16 +0530

    add pdf folder

--- controllers/pdf_controller.js
+++ controllers/pdf_controller.js
@@ -1,37 +1,37 @@
-// const ejs = require('ejs');
-// const fs = require('fs');
-// const pdf = require('html-pdf');
-// const express = require('express');
-// const path = require('path');
-// var app = express();
-// app.set('view engine','ejs');
-// app.set('views',path.join(__dirname,'views'));
-// const AVATAR_PATH = path.join('/public/pdf');
-// function createPDFFile(htmlString, fileName, callback) {
-//     var options = {
-//         format: 'Letter',
+const ejs = require('ejs');
+const fs = require('fs');
+const pdf = require('html-pdf');
+const express = require('express');
+const path = require('path');
+var app = express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+const AVATAR_PATH = path.join('/public/pdf');
+function createPDFFile(htmlString, fileName, callback) {
+    var options = {
+        format: 'Letter',
        
-//     };
-//     /**
-//      * It will create PDF of that HTML into given folder.
-//      */ 
-//     pdf.create(htmlString, options).toFile(path.join(__dirname)+".."+AVATAR_PATH+ fileName, function (err, data) {
-//         if (err) return console.log(err);
-//         console.log(data);
+    };
+    /**
+     * It will create PDF of that HTML into given folder.
+     */ 
+    pdf.create(htmlString, options).toFile(path.join(__dirname)+".."+AVATAR_PATH+ fileName, function (err, data) {
+        if (err) return console.log(err);
+        console.log(data);
         
-//       });
-// }
-// //  var contents = fs.readFileSync(path.join(__dirname)+".."+'/views/resume.ejs', 'utf8');
-// var html = ejs.render('resume.ejs', {data: 'some data'});
+      });
+}
+//  var contents = fs.readFileSync(path.join(__dirname)+".."+'/views/resume.ejs', 'utf8');
+var html = ejs.render('resume.ejs', {data: 'some data'});
 
-// createPDFFile(html,'output.pdf', function (err, result) {
-//     if (err) {
-//           console.log(err);
-//       } else { 
-//           console.log("PDF URL ADDED.");
-//           console.log(result);
-//       }
-//    });
+createPDFFile(html,'output.pdf', function (err, result) {
+    if (err) {
+          console.log(err);
+      } else { 
+          console.log("PDF URL ADDED.");
+          console.log(result);
+      }
+   });
 
 
 //second code
