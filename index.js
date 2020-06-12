dad7740196b7cdff1d3f0eefe61ad4e52d1644ee
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-12 17:39:55 +0530

    update comma

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ app.post('/',function(req,res){
         var main_str = user.skills;
         var str = req.body.queryResult.queryText;
         if(main_str.includes(",")){
-          query = main_str.replace(str+", ", "");
+          query = main_str.replace(str+",", "");
           query = main_str.replace(", " + str, "");
         }
         else
