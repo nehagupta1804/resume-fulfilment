389a2e8150b19623d8caacbb8fef32d12156c7df
renugupta2311 <gupta.neha1804@gmail.com>
2020-06-15 13:57:32 +0530

    add getName controller

---
+++ controllers/getname_controller.js
@@ -0,0 +1,117 @@
+module.exports.getName = function(req,res)
+{
+    let nextRes, query;
+        var name = req.body.queryResult.parameters["namelist"]["given-name"];
+        if (name === undefined)
+            query = req.body.queryResult.parameters["namelist"];
+        else
+            query = req.body.queryResult.parameters["namelist"]["given-name"];
+        let flag;
+        let contexts = req.body.queryResult.outputContexts;
+        console.log(contexts);
+        for (let i = 0; i < contexts.length; i++) {
+            var context = contexts[i];
+            if (context.name.endsWith('flag')) {
+                flag = context.parameters.flag;
+                break;
+            }
+        }
+        console.log(flag);
+        if (flag == "add") {
+            let id;
+            let contexts = req.body.queryResult.outputContexts;
+            console.log(contexts);
+            for (let i = 0; i < contexts.length; i++) {
+                var context = contexts[i];
+                if (context.name.endsWith('updateid')) {
+                    id = context.parameters.updateid;
+                    break;
+                }
+            }
+            console.log(id);
+            User.findByIdAndUpdate(id, {
+                "name": query
+            }, function(err, user) {
+                if (err) {
+                    console.log("cant be updated");
+                    return;
+                }
+                console.log("updated");
+            });
+            nextRes = "Resume Updated";
+            return res.json(200, {
+                "fulfillmentMessages": [{
+                    "platform": "ACTIONS_ON_GOOGLE",
+                    "simpleResponses": {
+                        "simpleResponses": [{
+                            "textToSpeech": [nextRes]
+                        }]
+                    }
+                }],
+                "outputContexts": [{
+                            "name": req.body.session + "/contexts/flag",
+                            "lifespanCount": 5,
+                            "parameters": {
+                                "flag": "create"
+                            }
+                        }
+
+                    ]                
+
+            });
+
+        } else {
+            console.log(query);
+            User.create({
+                name: query,
+                email: " ",
+                education: [],
+                experience: [],
+                project: [],
+                skills: " ",
+                interests: " ",
+                achievements: " "
+            }, function(err, user) {
+
+                if (err) {
+                    console.log("Error");
+                    return;
+                }
+                console.log(" user created \n");
+                var id = user._id;
+                console.log(id);
+                nextRes = "Please enter email";
+                return res.json(200, {
+                    "fulfillmentMessages": [{
+                        "platform": "ACTIONS_ON_GOOGLE",
+                        "simpleResponses": {
+                            "simpleResponses": [{
+                                "textToSpeech": [nextRes]
+                            }]
+                        }
+                    }],
+                    "outputContexts": [{
+                            "name": req.body.session + "/contexts/id",
+                            "lifespanCount": 5,
+                            "parameters": {
+                                "id": JSON.stringify(id)
+                            }
+                        },
+                        {
+                            "name": req.body.session + "/contexts/flag",
+                            "lifespanCount": 5,
+                            "parameters": {
+                                "flag": "create"
+                            }
+                        }
+
+                    ]
+                });
+
+
+            });
+        }
+
+
+
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("./config/mongoose");
 const User = require('./models/user');
 const https = require('https');
+const getname_controller = require('./controllers/getname_controller');
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
@@ -29,117 +30,9 @@ app.post('/', function(req, res) {
     var action = req.body.queryResult.action;
     console.log('action: ' + action);
     if (action == "getName") {
-        let nextRes, query;
-        var name = req.body.queryResult.parameters["namelist"]["given-name"];
-        if (name === undefined)
-            query = req.body.queryResult.parameters["namelist"];
-        else
-            query = req.body.queryResult.parameters["namelist"]["given-name"];
-        let flag;
-        let contexts = req.body.queryResult.outputContexts;
-        console.log(contexts);
-        for (let i = 0; i < contexts.length; i++) {
-            var context = contexts[i];
-            if (context.name.endsWith('flag')) {
-                flag = context.parameters.flag;
-                break;
-            }
-        }
-        console.log(flag);
-        if (flag == "add") {
-            let id;
-            let contexts = req.body.queryResult.outputContexts;
-            console.log(contexts);
-            for (let i = 0; i < contexts.length; i++) {
-                var context = contexts[i];
-                if (context.name.endsWith('updateid')) {
-                    id = context.parameters.updateid;
-                    break;
-                }
-            }
-            console.log(id);
-            User.findByIdAndUpdate(id, {
-                "name": query
-            }, function(err, user) {
-                if (err) {
-                    console.log("cant be updated");
-                    return;
-                }
-                console.log("updated");
-            });
-            nextRes = "Resume Updated";
-            return res.json(200, {
-                "fulfillmentMessages": [{
-                    "platform": "ACTIONS_ON_GOOGLE",
-                    "simpleResponses": {
-                        "simpleResponses": [{
-                            "textToSpeech": [nextRes]
-                        }]
-                    }
-                }],
-                "outputContexts": [{
-                            "name": req.body.session + "/contexts/flag",
-                            "lifespanCount": 5,
-                            "parameters": {
-                                "flag": "create"
-                            }
-                        }
 
-                    ]                
-
-            });
-
-        } else {
-            console.log(query);
-            User.create({
-                name: query,
-                email: " ",
-                education: [],
-                experience: [],
-                project: [],
-                skills: " ",
-                interests: " ",
-                achievements: " "
-            }, function(err, user) {
-
-                if (err) {
-                    console.log("Error");
-                    return;
-                }
-                console.log(" user created \n");
-                var id = user._id;
-                console.log(id);
-                nextRes = "Please enter email";
-                return res.json(200, {
-                    "fulfillmentMessages": [{
-                        "platform": "ACTIONS_ON_GOOGLE",
-                        "simpleResponses": {
-                            "simpleResponses": [{
-                                "textToSpeech": [nextRes]
-                            }]
-                        }
-                    }],
-                    "outputContexts": [{
-                            "name": req.body.session + "/contexts/id",
-                            "lifespanCount": 5,
-                            "parameters": {
-                                "id": JSON.stringify(id)
-                            }
-                        },
-                        {
-                            "name": req.body.session + "/contexts/flag",
-                            "lifespanCount": 5,
-                            "parameters": {
-                                "flag": "create"
-                            }
-                        }
-
-                    ]
-                });
-
-
-            });
-        }
+        return (getname_controller.getName(req,res));
+       
 
     } else if (action == "getEmail") {
 
